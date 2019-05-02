fb566110707a401a2e97ca2eaac50ae892f11dc3
Root <navaneeth.94@gmail.com>
2019-05-02 15:56:30 +0200

    included basic waypoint nav

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #Main code to begin mission
+from dronekit import connect
+
+# Connect to UDP endpoint.
+vehicle = connect('127.0.0.1:14550', wait_ready=False)
+vehicle.wait_ready(True, timeout=300)
+# Use returned Vehicle object to query device state - e.g. to get the mode:
+print("Mode: %s" % vehicle.mode.name)
